09ccf7d9a1326d6922bccab0928f6531508958d7
hpriyap <phema@seas.upenn.edu>
2023-11-14 12:02:42 -0500

    trial2

--- foodapi.py
+++ foodapi.py
@@ -1,1 +1,26 @@
 print("This is API part of the code")
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import json
+import requests
+# Define the API endpoint
+url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+# Define the headers for the API request
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+
+# Define the data for the API request
+data = {
+    "prompt": "Suggest 5 recipes with the following ingredients: '{}'",
+    "max_tokens": 60
+}
+
+# Make the API request
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# Print the response
+print(response.json())
+
