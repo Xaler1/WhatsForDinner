015178c2398b25af89667469aef0718ef1444a01
hpriyap <phema@seas.upenn.edu>
2023-11-16 13:42:38 -0500

    API

--- foodapi.py
+++ foodapi.py
@@ -12,9 +12,15 @@ headers = {
     "Authorization": "Bearer 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"
 }
 
-# Define the data for the API request
+# Ask the user for the level of difficulty of the recipe (easy, medium, hard) and assign it a variable
+difficulty = input("What level of difficulty would you like your recipe to be? (easy, intermediate, master) ")
+
+# Ask the user for the type of cuisine of the recipe (Thai, Italian, Mexican) and assign it a variable
+cuisine = input("What type of cuisine would you like your recipe to be? (Thai, Italian, Mexican, American, Indian, Chinese, ...) ")
+
+# Define the data for the API request with the user's input
 data = {
-    "prompt": "Suggest 5 recipes with the following ingredients: '{}'",
+    "prompt": "Given me 10 '{difficulty}' level '{cuisine}' recipes in step-by-step detail for the following list of ingredients: '{{}}'",
     "max_tokens": 60
 }
 
